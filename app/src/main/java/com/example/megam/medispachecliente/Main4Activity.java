2141d7abd2f27ab2cfa87aa2156a53d042579fc9
João Pedro Santos <betajpsg@gmail.com>
2018-12-17 23:20:38 -0300

    alteracao na desgraca da cardview

--- app/src/main/java/com/example/megam/medispachecliente/Main4Activity.java
+++ app/src/main/java/com/example/megam/medispachecliente/Main4Activity.java
@@ -2,8 +2,12 @@ package com.example.megam.medispachecliente;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.*;
+import android.support.v7.widget.CardView;
 
 public class Main4Activity extends AppCompatActivity {
+
+    private CardView mycardview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
